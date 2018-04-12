d8aa76d4009db0f2307720e8fb975807c1ad5d6f
kgantsho <boikgantshoramokosi@gmail.com>
2018-04-12 23:14:54 +0200

    fix NormalMoves to pass all test, check if the current position is null

--- Checkers/Piece.cs
+++ Checkers/Piece.cs
@@ -16,10 +16,45 @@ namespace Checkers
             throw new NotImplementedException();
         }
 
-       virtual public IEnumerable<int> NormalMoves(IBoard board)
+
+        public virtual IEnumerable<int> NormalMoves(IBoard b)
         {
-            throw new NotImplementedException();
+            List<int> result = new List<int>();
+            int col = (Position - 1) % 4;
+            int row = (Position - 1) / 4;
+            bool sideSpace = (col == 0 && row % 2 == 1) || (col == 3 && row % 2 == 0);
+            if (Color == Color.Black)
+            {
+                int next = Position + 3;
+                if (row % 2 == 0 || col == 0) next++;
+                if (next <= 32 && b.Occupant(next) == null)
+                {
+                    result.Add(next);
+                }
+                next++;
+                if (!sideSpace && next <= 32 && b.Occupant(next) == null)
+                {
+                    result.Add(next);
+                }
+            }
+            else
+            {
+                int next = Position - 3;
+                if (row % 2 == 1 || col == 3) next--;
+                if (next >= 1 && b.Occupant(next) == null)
+                {
+                    result.Add(next);
+                }
+                next--;
+                if (!sideSpace && next >= 1 && b.Occupant(next) == null)
+                {
+                    result.Add(next);
+                }
+            }
+            return result;
         }
+
+
 
         public Piece(Color c, int pos)
         {
